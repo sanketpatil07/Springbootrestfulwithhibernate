ae439ef1b0412574465f41c01449e63f8fbd241c
sanket07 <sanketpadole218@gmail.com>
2022-08-11 17:34:45 +0530

    SBCWR : project created

---
+++ src/main/java/com/example/demo/SpringbootrestfulwithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestfulwithHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestfulwithHibernateApplication.class, args);
+	}
+
+}
